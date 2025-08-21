cc58414c3e14b49fcbb697e609e5d07cd7276dfb
TingHao <howard92419@gmail.com>
2025-08-21 12:40:31 +0800

    Modify the prompt of read image

--- api/index.py
+++ api/index.py
@@ -134,6 +134,8 @@ def handle_message(event):
 @web_handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
     try:
+        '''因為傳完圖片GPT不能知道我傳圖片要幹什麼，
+        這邊採用主動問答的方式，讓GPT知道我要他做啥'''
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="請問你需要我幫你做什麼")
